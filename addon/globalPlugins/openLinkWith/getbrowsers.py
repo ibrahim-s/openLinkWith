162748f90fc02ac5548e72240933383c9bb18c6a
ibrahim <ibra.hamadeh@hotmail.com>
2017-11-27 11:46:28 +0200

    make getbrowsers search for browsers in the registry

--- addon/globalPlugins/openLinkWith/getbrowsers.py
+++ addon/globalPlugins/openLinkWith/getbrowsers.py
@@ -1,25 +1,35 @@
-#this module tries to get several browsers: Chrome, Firefox, Internet explorer, Opera, SeaMonkey, and Wyzo if found on pc.
-
+_winreg=None
 import os
+import sys
+if sys.version.startswith('3'):
+	import winreg
+	_winreg= winreg
+elif sys.version.startswith('2'):
+	import _winreg
+	_winreg= _winreg
 
-possiblePaths=[
-('Firefox', ('c:\\program files\\mozilla firefox\\firefox.exe', 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')),
-('Google Chrome', ('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
-'C:\\Program Files\\Google\\Application\\chrome.exe', 'C:\\Program Files (x86)\\Google\\Application\\chrome.exe')),
-('Internet Explorer', ('C:\\Program Files\\Internet Explorer\\IEXPLORE.EXE', 'C:\\Program Files (x86)\\Internet Explorer\\IEXPLORE.EXE')),
-('Opera', ('C:\\Program Files\\Opera\\launcher.exe', 'C:\\Program Files (x86)\\Opera\\launcher.exe')),
-('Wyzo', ('C:\\Program Files\\Wyzo\\wyzo.exe', 'C:\\Program Files (x86)\\Wyzo\\wyzo.exe')),
-('Seamonkey', ('C:\\Program Files\\SeaMonkey\\seamonkey.exe', 'C:\\Program Files (x86)\\SeaMonkey\\seamonkey.exe')),
-]
+#these are the keys that we are going to look for in the registry
+exeLabels= {'firefox.exe': 'Firefox', 'chrome.exe': 'Google Chrome', 'iexplore.exe': 'Internet Explorer', 'opera.exe': 'Opera', 'seamonkey.exe': 'Seamonkey', 'wyzo.exe': 'Wyzo'}
+def getKeyRegBrowsers(base_key, sub_key):
+	"""sub_key is one of the main keys in the registry, or defined constants in _winreg module
+	sub_key is the sub key in  the registry under which executable labels is found
+	this function will return a list of tuples, consisting of the browser label if present and its path.""" 
+	foundBrowsers=[]
+	try:
+		key= _winreg.OpenKey(base_key, sub_key)
+		i=0
+		while True:
+			foundKey= _winreg.EnumKey(key, i)
+			if foundKey.lower() in exeLabels:
+				key2= _winreg.OpenKey(base_key, os.path.join(sub_key, foundKey))
+				#_winreg.EnumValue(key2, 0) returns a tuple of three elements, path is the second.
+				foundBrowsers.append((exeLabels[foundKey.lower()], _winreg.EnumValue(key2, 0)[1]))
+			i+=1
+	except WindowsError:
+		pass
+	return foundBrowsers
 
 def getBrowsers():
-	""" return a list of browsers and their path
-	if present in computer. """
-	browsers=[]
-#getting browsers in program files or Program Files (x86)
-	for browser,paths in possiblePaths:
-		for path in paths:
-			if os.path.exists(path):
-				browsers.append((browser,path))
-				break
-	return browsers
+	"""returns a list of browsers and their path in both
+	HKEY_LOCAL_MACHINE an HKEY_CURRENT_USER main keys in the registry."""
+	return getKeyRegBrowsers(_winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths')+ getKeyRegBrowsers(_winreg.HKEY_CURRENT_USER,r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths')
