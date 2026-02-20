fadb90793c13af467a665f3c57922dd65e23abed
Ibrahim Hamadeh <ibra.hamadeh@hotmail.com>
2026-02-20 11:20:40 +0200

    Remove code related to versionInfo, for it is not needed.

--- addon/globalPlugins/openLinkWith/getlinks.py
+++ addon/globalPlugins/openLinkWith/getlinks.py
@@ -7,31 +7,22 @@ import re
 import api, ui
 import speech
 import speechViewer
-import versionInfo
 
 import addonHandler
 addonHandler.initTranslation()
 
 class LastSpoken:
 	''' Helper class that contains the code, to get last spoken text.'''
-	BUILD_YEAR = getattr(versionInfo, 'version_year', 2021)
 	lastSpokenText=None
 
 	@classmethod
 	def _patch(cls):
-		if cls.BUILD_YEAR >= 2021:
-			cls.oldSpeak = speech.speech.speak
-			speech.speech.speak = cls.mySpeak
-		else:
-			cls.oldSpeak = speech.speak
-			speech.speak = cls.mySpeak
+		cls.oldSpeak = speech.speech.speak
+		speech.speech.speak = cls.mySpeak
 
 	@classmethod
 	def terminate(cls):
-		if cls.BUILD_YEAR >= 2021:
-			speech.speech.speak = cls.oldSpeak
-		else:
-			speech.speak = cls.oldSpeak
+		speech.speech.speak = cls.oldSpeak
 
 	@classmethod
 	def mySpeak(cls, sequence, *args, **kwargs):
